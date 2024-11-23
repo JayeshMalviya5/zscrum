01b4ee0ff3512408bc0ebf5bd00f05b6574eed89
JayeshMalviya5 <115400817+JayeshMalviya5@users.noreply.github.com>
2024-11-23 19:26:11 +0530

    changes

---
+++ components/header.jsx
@@ -0,0 +1,44 @@
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
+
+function Header() {
+  return (
+    <header>
+      <nav className={"py-6 px-4 flex justify-between items-center"}>
+        <Link href="/">
+          <Image
+            src={"/logo2.png"}
+            alt="Zscrum logo"
+            width={200}
+            height={56}
+            className={"h-10 w-auto object-contain"}
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
